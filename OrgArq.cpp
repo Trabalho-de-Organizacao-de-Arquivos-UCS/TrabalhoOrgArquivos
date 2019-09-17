94d5c93b7d77206660d7ba9c12b12d8bced91ba1
Gabriel Rodrigues <42683967+GabrielSRodriguesBR@users.noreply.github.com>
2019-09-17 10:35:20 -0300

    ...

--- OrgArq.cpp
+++ OrgArq.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <Windows.h>
 struct twitter{
 	
 	int id_twitter;
@@ -12,7 +13,62 @@ struct twitter{
 	
 };
 
+char* createDir(){
+	if(!CreateDirectory("c:\\temp\\OrgArq", NULL)) {
+        return NULL;
+  	}
+  	else{
+  		FILE *file;
+  		
+  		if(file = fopen("c:\\temp\\OrgArq\\file.dat","wb+")){
+		 return "c:\\temp\\OrgArq\\file.dat";
+		}
+		else{
+			 return NULL;
+		}
+	}
+    
+}
+
 
 main(){
+	
+	char *dir = createDir();
+	FILE *file;
+	
+	struct twitter *t_object = (twitter*) malloc(sizeof(struct twitter));
+	strcpy(t_object->usuario, "User1");
+	strcpy(t_object->hashtag, "#Joker");
+	strcpy(t_object->data, "17/09/2019");
+	strcpy(t_object->mensagem, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua");
+	strcpy(t_object->pais, "Brazil");
+	strcpy(t_object->usuario, "User1");
+	
+	if(file = fopen("c:\\temp\\OrgArq\\file.dat","ab")){
+		 fwrite(t_object, sizeof(struct twitter), 1, file);
+	}
+	
+	strcpy(t_object->usuario, "User2");
+	strcpy(t_object->hashtag, "#Rambo");
+	strcpy(t_object->data, "19/09/2019");
+	strcpy(t_object->mensagem, "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua");
+	strcpy(t_object->pais, "Brazil");
+	strcpy(t_object->usuario, "User2");
+	
+	if(file = fopen("c:\\temp\\OrgArq\\file.dat","ab")){
+		 fwrite(t_object, sizeof(struct twitter), 1, file);
+	}
+	
+	if(file = fopen("c:\\temp\\OrgArq\\file.dat","rb")){
+
+		while(!feof(file)){
+		fread(t_object, sizeof(struct twitter), 1, file);
+		printf("%s, %s \n",t_object->usuario,t_object->mensagem); 
+		}
+
+		 
+	}
+	
+	
 	
 }
