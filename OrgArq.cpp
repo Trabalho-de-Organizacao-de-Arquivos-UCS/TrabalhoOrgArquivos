464b3d4a4fd30032ea244c68745414b04deaaea1
Gabriel Rodrigues <gsrodrigues2@ucs.br>
2019-09-16 23:05:29 -0300

    Criacao OrgArq.cpp

---
+++ OrgArq.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct ceplog{
+	
+	int id_twitter;
+	char usuario[20];
+	char mensagem[280];
+	char data[8];
+	char pais[20];
+	char hashtag[200];
+	
+	
+};
+
+
+main(){
+	
+}
