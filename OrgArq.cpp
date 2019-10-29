91b731a8bdfe2d0501f73b96707571fbe26aee74
Gabriel Rodrigues <42683967+GabrielSRodriguesBR@users.noreply.github.com>
2019-10-29 17:55:34 -0300

    index user

--- OrgArq.cpp
+++ OrgArq.cpp
@@ -20,6 +20,12 @@ struct index{
 
 };
 
+struct usuario_index{
+	char usuario[20];
+	long int pos_in_file;
+
+};
+
 char* createDir(){
 	if(!CreateDirectory("c:\\temp\\OrgArq", NULL)) {
         return NULL;
@@ -108,6 +114,37 @@ void indexar(struct twitter *tw, struct index *ix){
 
 }
 
+
+void indexarByUser(struct twitter *tw, struct usuario_index *ix){
+	FILE *file, *index;
+	struct usuario_index *aux_index = (struct usuario_index *)malloc(sizeof(struct usuario_index));
+	int posicao_index = 0;
+	if(file = fopen("c:\\temp\\OrgArq\\file.dat","rb")){
+	
+		while(!feof(file)){
+			
+			fread(tw, sizeof(struct twitter), 1, file);
+		 
+			if(index = fopen("c:\\temp\\OrgArq\\index_user.dat","ab")){
+				if(strlen(tw->usuario) <= 20){				
+					aux_index->pos_in_file = ftell(file);
+					strcpy(aux_index->usuario, tw->usuario);
+					fwrite(aux_index,sizeof(struct usuario_index),1,index);
+					printf("NOVO REGISTRO INSERIDO NO INDEX: %s \n",aux_index->usuario);
+				}
+			
+			}
+			
+			fclose(index);
+			
+		}
+		
+	}
+	
+	
+
+}
+
 void displayAll(struct twitter *tw){
 
 	FILE *file;
@@ -135,7 +172,9 @@ void LaunchMenu(){
 	struct twitter *t_object = (struct twitter*) malloc(sizeof(struct twitter));
 	struct index *t_index = (struct index *)malloc(sizeof(struct index));
 	
-	char menu[500] = "\nSelecione a opcao abaixo\n1- Listar todos os registros\n2- Indexar Arquivo\n3- Pesquisar Hashtag\nInforme a opcao: ";
+    struct usuario_index *u_index = (struct usuario_index *)malloc(sizeof(struct usuario_index));
+	
+	char menu[500] = "\nSelecione a opcao abaixo\n1- Listar todos os registros\n2- Indexar Arquivo (HashTag)\n3- Indexar Arquivo (Usuario)\n4- Pesquisar Hashtag\nInforme a opcao: ";
     printf("\n%s\n",menu);
 	bool exit = false;
 	while(!exit){
@@ -155,6 +194,10 @@ void LaunchMenu(){
 				printf("\n%s\n",menu);
 			break;
 			case 3:
+				indexarByUser(t_object,u_index);
+				printf("\n%s\n",menu);
+			break;
+			case 4:
 				char term[200];
 				printf("\nInforme a hashtag:\n");
 				scanf("%s",&term);
